c0c4de584fe3bcece947482408aad775f80499e2
danurrizad <danurrizad242@gmail.com>
2022-11-08 23:12:14 +0700

    change logo ugm and update Header

--- frontendlab/src/components/Header.jsx
+++ frontendlab/src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import './Header.css';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import logoImage from "../assets/images/logo-ugm.png"
 
 export default function Header(){
     return (
-        <div class="header">
-            <img src="images\logo-ugm.png" alt="logo-ugm"/>
-            <h1>Departemen Teknik Elektro <br></br>dan Teknologi Informasi<br></br>Universitas Gadjah Mada</h1>
+        <div>
+            <Navbar className="header">
+                <Container fluid>
+                    <Navbar.Brand>
+                        <img className="logoImage" src={logoImage} alt="logo-ugm" />
+                        <Nav className="titleNav">
+                            Departemen Teknik Elektro dan <br></br> Teknologi Informasi <br></br> Universitas Gadjah Mada
+                        </Nav> 
+                    </Navbar.Brand>
+                </Container>
+
+            </Navbar>
         </div>
     )
 }
