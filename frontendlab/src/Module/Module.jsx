d49838dae921334794253f6d0a0af4c6ec138900
danurrizad <danurrizad242@gmail.com>
2022-11-08 23:15:52 +0700

    add initial Profile, Module, and Schedule

---
+++ frontendlab/src/Module/Module.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Module.css';
+
+export default function Module(){
+    return(
+        <>
+            <div class="module">
+                <h1>Module</h1>
+            </div>
+        </>
+    )
+}
