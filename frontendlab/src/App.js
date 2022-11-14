bc4185da54991ec6337f07d2fc0a73e9966ce306
ciciliawidhiii <ciciliawidhi1@gmail.com>
2022-11-14 07:44:40 +0700

    Add Login Page

--- frontendlab/src/App.js
+++ frontendlab/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Header/>
+      <Login/>
     </div>
   );
 }
