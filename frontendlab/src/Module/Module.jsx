9e21bcd986248b4b24c36d1238ee43996fe0972e
faizahbestiyana <faizahbestiyana@gmail.com>
2022-11-16 20:39:59 +0700

    add module card

--- frontendlab/src/Module/Module.jsx
+++ frontendlab/src/Module/Module.jsx
@@ -4,8 +4,19 @@ import './Module.css';
 export default function Module(){
     return(
         <>
-            <div class="module">
-                <h1>Module</h1>
+            <div className="praktikum-container">
+                <h1 className="module-heading">Module</h1>
+                <div className="module-container">
+                    <div className="module-card">
+                    <h2 className="module-title"> Module 
+                    Title</h2>
+                    <div className="module-details"> 
+                        <p> This is text </p>
+                        <div className="mo-btns"> View details
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>
         </>
     )
