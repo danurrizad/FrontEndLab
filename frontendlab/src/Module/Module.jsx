5ed8430a63cdecae08056ae01474a393bc2e3c2e
ciciliawidhiii <ciciliawidhi1@gmail.com>
2022-11-17 23:41:12 +0700

    fix connection

--- frontendlab/src/Module/Module.jsx
+++ frontendlab/src/Module/Module.jsx
@@ -1,14 +1,32 @@
-import {React} from "react";
-import {Link} from 'react-router-dom';  
-import './Module.css';
-//import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { HiFilter } from "react-icons/hi";
+import axios from "axios";
+import { Link} from "react-router-dom";
 
 
+const Module = () => {
+    const [module, setModul] = useState([]);
+      useEffect(() => {
+        getModule();
+      }, []);
+    
+      const getModule = async () => {
+        const response = await axios.get("http://api-paw.bekisar.net/api/v1/modules");
+        setModul(response.data.data);
+      }; 
 
-export default function Module(){
+      const deleteModule = async (title) => {
+        try {
+          await axios.delete(`"http://api-paw.bekisar.net/api/v1/modules/633681c7041eb46a1ca51939/${title}`);
+          getModule();
+        } catch (error) {
+          console.log(error);
+        }
+      };
     return(
-        <>
-            <div class="module">
+            <>
+                <div class="module">
                 <div className="title">
                     <h1>Modules</h1>
                 </div>
@@ -20,7 +38,8 @@ export default function Module(){
                                 <th width="350px" >Nama Modul</th>
                                 <th width="150px" >Batch</th>
                                 <th width="150px" >Hari</th>
-                                <th width="250px" >Tempat</th>
+                                <th width="250px" >Lab</th>
+                                <th width="250px" >Semester</th>
                                 <th width="250px" >Tanggal</th>
                                 <th width="250px" >Kuota</th>
                             </tr>
@@ -43,6 +62,50 @@ export default function Module(){
                     </Link>
                 </div>
             </div>
-        </>
-    )
-}
+                <tbody>
+                    {module.map((dat) => {
+                        if (dat.title === title) {
+                            return (
+                                <tr
+                                    key={dat.title}
+                                >
+                                <td>
+                                    {dat.batch}
+                                </td>
+                                <td>
+                                    {dat.day}
+                                </td>
+                                <td>
+                                    {dat.lab}
+                                </td>
+                                <td>
+                                    {dat.semester}
+                                </td>
+                                <td>
+                                    {dat.dateStart}
+                                </td>
+                                <td>
+                                    {dat.quota}
+                                </td>
+                                <td>
+                                    {" "}
+                                    <Link
+                                        to={`editmodule/${dat.title}`}
+                                    >
+                                        Edit
+                                    </Link>
+                                    <button
+                                        onClick={() => deleteModule(dat.title)}
+                                    >
+                                        Delete
+                                    </button>
+                                    </td>
+                                </tr>
+                                  );
+                                }
+                              })}
+                </tbody>    
+            </>
+        );
+    };
+export default Module;
---
+++ frontendlab/tailwind.config.js
@@ -0,0 +1,16 @@
+/* @type {import('tailwindcss').Config}
+module.exports = {
+  content: [
+    "./src/Module/Module.jsx",
+    "./src/Profile/Profile.jsx",
+    "./src/Schedule/Schedule.jsx",
+    "./src/Homepage/Homepage.jsx",
+    "./src/components/Header.jsx",
+    "./src/components/Sidenav.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+*/
