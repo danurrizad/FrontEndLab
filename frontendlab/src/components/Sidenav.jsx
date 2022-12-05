19e5576c1d3fd82a11a5a76e9cd8ed5d383a75be
danurrizad <danurrizad242@gmail.com>
2022-12-06 02:34:37 +0700

    update for using tailwind css

--- frontendlab/src/components/Sidenav.jsx
+++ frontendlab/src/components/Sidenav.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
-import './Sidenav.css'
-import {NavLink} from 'react-router-dom'
-import {FaCalendarAlt, FaUserAlt, FaDiceD6, FaBars, FaHome, FaSignOutAlt} from "react-icons/fa"
+import Modal from './Modal'
+import {NavLink, useNavigate} from 'react-router-dom'
+import {FaUserAlt, FaDiceD6, FaBars, FaHome, FaSignOutAlt} from "react-icons/fa"
+import LogoDashboard from "../assets/images/logo-dteti-dashboard.png"
+import axios from 'axios'
 
 
 
 const Sidenav = ({children}) => {
     const[isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen (!isOpen);
+    const navigate = useNavigate();
+    const [showModal, setShowModal] = useState({
+        message:"",
+        isLoading:false
+      });
     const menuItem =[
         {
             path:'/home',
@@ -24,37 +31,68 @@ const Sidenav = ({children}) => {
             name:'Module',
             icon:<FaDiceD6/>
         },
-        /*{
-            path:'/schedule',
-            name:'Schedule',
-            icon:<FaCalendarAlt/>
-        },*/
-        
     ]
+
+    const Logout = async() => {
+        try{
+            await axios.post('http://api-paw.bekisar.net/api/v1/auth/logout');
+            navigate("/login")
+        } catch(error){
+            console.log(error);
+        }
+    }
+    
+    const handleModal = (message, isLoading) => {
+        setShowModal({
+          message,
+          isLoading,
+        })
+      }
+    const handleLogout = () => {
+        handleModal("Are you sure want to logout?", true);
+    }
+    const confirmLogout = (yes) => {
+        if(yes){
+            Logout();
+        }
+        else{
+            handleModal("", false);
+        }
+
+    }
+
     return (
-       <div className='containerNav'>
-         <div style={{width: isOpen ? "300px" : "50px"}} className="sidenav">
-             <div className="top_section">
-                 <h1 style={{display: isOpen ? "block" : "none"}} className='userSidebar'>Dashboard</h1>
-                 <div style={{marginLeft: isOpen ? "100px" : "0"}} className="bars">
-                    <FaBars onClick={toggle}/>
-                 </div>
+       <div className='flex m-auto p-0'> 
+         <div style={{width: isOpen ? "50px" : "300px"}} className="bg-[#6fb3b8] text-white h-[100vh] w-[300px] transition-all">
+             <div className="flex items-center py-[20px] px-[15px]">
+                <img src={LogoDashboard} style={{display: isOpen ? "none" : "block"}} className="flex w-[85%]"></img>
+                <div style={{marginLeft: isOpen ? "0" : "10px"}} className="flex text-[25px] mt-[20px]">
+                <FaBars onClick={toggle}/>
+                </div>    
+             </div>
+             <div>
+                <button class="pointer-events-none">_____________________________________</button>
              </div>
              {
                 menuItem.map((item, index)=>(
-                    <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                        <div className="icon">{item.icon}</div>
-                        <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                    <NavLink to={item.path} key={index} className="flex text-current decoration-transparent py-[10px] px-[15px] gap-[15px] hover:bg-[#badfe7] hover:text-[#388087] focus:bg-[#badfe7] focus:text-[#388087]">
+                        <div className="text-[20px] py-2.5">{item.icon}</div>
+                        <div style={{display: isOpen ? "none" : "block"}} className="text-[20px] py-1">{item.name}</div>
                     </NavLink>
                 )) 
              }
-             <NavLink to={'/logout'} className="linkBottom">
-                <div className="icon">{<FaSignOutAlt/>}</div>
-                <div style={{display: isOpen ? "block" : "none"}} className="link_text">{'Logout'}</div>
+             <div>
+                <button class="pointer-events-none">_____________________________________</button>
+             </div>
+             <NavLink onClick={handleLogout} className="flex text-current decoration-transparent py-[10px] px-[15px] gap-[15px] hover:bg-[#ffb4b3] hover:text-[#c91212] hover:font-bold focus:bg-[#ffb4b3] focus:text-[#c91212] focus:font-bold">
+                <div className="text-[20px] py-2.5">{<FaSignOutAlt/>}</div>
+                <div style={{display: isOpen ? "none" : "block"}} className="text-[20px] py-1" >Logout</div>
              </NavLink>
          </div>
-         <main>{children}</main>
+         {showModal.isLoading && <Modal onDialog={confirmLogout} message={showModal.message}/>}
+         <main className='w-[100%]'>{children}</main>
        </div>
+       
     )
 }
 
