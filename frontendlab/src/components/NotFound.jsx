210b2241fb4548284a723ce695d962badfcd8f9c
danurrizad <danurrizad242@gmail.com>
2022-12-06 02:32:26 +0700

    add handling for missing pages

---
+++ frontendlab/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return(
+        <div className="text-center h-screen">
+            <h2 class="">404 Page Not Found </h2>
+            <p>Page that you search for is not found here...</p>
+            <Link to="/home">Back to home</Link>
+        </div>
+    )
+}
+
+export default NotFound;
