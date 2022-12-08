3560338aee84d173e12f1cd1a30c32dd181d2ec8
tasyanafisah <tasyanafisah02@mail.ugm.ac.id>
2022-12-09 00:52:08 +0700

    Update style

--- frontendlab/src/pages/Module/EditModule.jsx
+++ frontendlab/src/pages/Module/EditModule.jsx
@@ -81,144 +81,146 @@ function EditModul() {
   };
 
   return (
-      <div className="crud">
-        <div className="content-center items-center">
-          <center><h1 className="font-bold pb-4">Edit Module</h1></center>
-          <div className="flex-col w-full h-140 rounded-md shadow-md bg-white p-7">
-            <form className="flex flex-col gap-2" onSubmit={saveModule}>
-              <div className="flex flex-col">
-                <label
-                  className="text-base font-medium text-gray"
-                  htmlFor="Nama Modul"
-                >
-                  Nama Modul
-                </label>
-                <input
-                  className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                  id="title"
-                  type="text"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                  name="title"
-                  placeholder="Masukkan Nama Modul"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label
-                  className="text-base font-medium text-gray"
-                  htmlFor="Batch"
-                >
-                  Batch
-                </label>
-                <input
-                  className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                  id="Batch"
-                  type="number"
-                  value={batch}
-                  onChange={(e) => setBatch(e.target.value)}
-                  name="Batch"
-                  placeholder="Masukkan Batch"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label
-                  className="text-base font-medium text-gray"
-                  htmlFor="Hari"
-                >
-                  Hari
-                </label>
-                <input
-                  className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                  id="Hari"
-                  type="number"
-                  value={day}
-                  onChange={(e) => setDay(e.target.value)}
-                  name="Hari"
-                  placeholder="Masukkan Hari dalam Angka (Senin = 1)"
-                />
-              </div>
-              <div className="flex flex-col">
+      <div className="bg-[#f6f6f2] min-h-screen p-10">
+        <div className="crud">
+          <div className="content-center items-center">
+            <center><h1 className="font-bold pb-4">Edit Module</h1></center>
+            <div className="flex-col w-full h-140 rounded-md shadow-md bg-white p-7">
+              <form className="flex flex-col gap-2" onSubmit={saveModule}>
+                <div className="flex flex-col">
                   <label
                     className="text-base font-medium text-gray"
-                    htmlFor="Lab"
+                    htmlFor="Nama Modul"
                   >
-                    Lab
+                    Nama Modul
                   </label>
                   <input
                     className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                    id="Lab"
+                    id="title"
                     type="text"
-                    value={lab}
-                    onChange={(e) => setLab(e.target.value)}
-                    name="Lab"
-                    placeholder="Masukkan Lab"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    name="title"
+                    placeholder="Masukkan Nama Modul"
                   />
                 </div>
                 <div className="flex flex-col">
                   <label
                     className="text-base font-medium text-gray"
-                    htmlFor="Semester"
+                    htmlFor="Batch"
                   >
-                    Semester
+                    Batch
                   </label>
                   <input
                     className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                    id="Semester"
+                    id="Batch"
                     type="number"
-                    value={semester}
-                    onChange={(e) => setSemester(e.target.value)}
-                    name="Semester"
-                    placeholder="Masukkan Semester"
+                    value={batch}
+                    onChange={(e) => setBatch(e.target.value)}
+                    name="Batch"
+                    placeholder="Masukkan Batch"
                   />
                 </div>
                 <div className="flex flex-col">
                   <label
                     className="text-base font-medium text-gray"
-                    htmlFor="Tanggal"
+                    htmlFor="Hari"
                   >
-                    Tanggal
+                    Hari
                   </label>
                   <input
                     className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                    id="Tanggal"
-                    type="date"
-                    value={dateStart}
-                    onChange={(e) => setdateStart(e.target.value)}
-                    name="Tanggal"
-                    placeholder="Masukkan Tanggal Batch Modul Dilaksanakan"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <label
-                    className="text-base font-medium text-gray"
-                    htmlFor="Masukkan Kuota Batch"
-                  >
-                    Kuota
-                  </label>
-                  <input
-                    className="border-b-2 w-full p-1 text-gray-500 bg-white"
-                    id="Kuota"
+                    id="Hari"
                     type="number"
-                    value={quota}
-                    onChange={(e) => setQuota(e.target.value)}
-                    name="Kuota"
-                    placeholder="Masukkan kuota"
+                    value={day}
+                    onChange={(e) => setDay(e.target.value)}
+                    name="Hari"
+                    placeholder="Masukkan Hari dalam Angka (Senin = 1)"
                   />
                 </div>
-              <div className="buttonsModule justify-center flex items-center gap-3">
-                <button
-                  className="rounded-[50px] py-1 px-14 bg-[#388087] text-white hover:bg-cyan-900"
-                  type="submit"
-                >
-                  Edit
-                </button>
-                <Link to="/module">
-                  <button className="rounded-[50px] decoration-0 py-1 px-14 bg-[#388087] text-white hover:bg-cyan-900">
-                    Batal
+                <div className="flex flex-col">
+                    <label
+                      className="text-base font-medium text-gray"
+                      htmlFor="Lab"
+                    >
+                      Lab
+                    </label>
+                    <input
+                      className="border-b-2 w-full p-1 text-gray-500 bg-white"
+                      id="Lab"
+                      type="text"
+                      value={lab}
+                      onChange={(e) => setLab(e.target.value)}
+                      name="Lab"
+                      placeholder="Masukkan Lab"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      className="text-base font-medium text-gray"
+                      htmlFor="Semester"
+                    >
+                      Semester
+                    </label>
+                    <input
+                      className="border-b-2 w-full p-1 text-gray-500 bg-white"
+                      id="Semester"
+                      type="number"
+                      value={semester}
+                      onChange={(e) => setSemester(e.target.value)}
+                      name="Semester"
+                      placeholder="Masukkan Semester"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      className="text-base font-medium text-gray"
+                      htmlFor="Tanggal"
+                    >
+                      Tanggal
+                    </label>
+                    <input
+                      className="border-b-2 w-full p-1 text-gray-500 bg-white"
+                      id="Tanggal"
+                      type="date"
+                      value={dateStart}
+                      onChange={(e) => setdateStart(e.target.value)}
+                      name="Tanggal"
+                      placeholder="Masukkan Tanggal Batch Modul Dilaksanakan"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label
+                      className="text-base font-medium text-gray"
+                      htmlFor="Masukkan Kuota Batch"
+                    >
+                      Kuota
+                    </label>
+                    <input
+                      className="border-b-2 w-full p-1 text-gray-500 bg-white"
+                      id="Kuota"
+                      type="number"
+                      value={quota}
+                      onChange={(e) => setQuota(e.target.value)}
+                      name="Kuota"
+                      placeholder="Masukkan kuota"
+                    />
+                  </div>
+                <div className="buttonsModule justify-center flex items-center gap-3">
+                  <button
+                    className="rounded-[50px] py-1 px-14 bg-[#388087] text-white hover:bg-cyan-900"
+                    type="submit"
+                  >
+                    Edit
                   </button>
-                </Link>
-              </div>
-            </form>
+                  <Link to="/admin-module">
+                    <button className="rounded-[50px] decoration-0 py-1 px-14 bg-[#388087] text-white hover:bg-cyan-900">
+                      Batal
+                    </button>
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
