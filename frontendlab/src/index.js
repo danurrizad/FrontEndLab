b80ae67f49383c5bb2e0241298ad2989b89042f5
ciciliawidhiii <ciciliawidhi1@gmail.com>
2022-12-08 20:44:17 +0700

    Add cookies

--- frontendlab/src/index.js
+++ frontendlab/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CookiesProvider } from "react-cookie";
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
